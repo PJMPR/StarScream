24473851b369e69535b63aa74eb0848b5de11656
ProfSJonalista <jan.mietki@gmail.com>
2016-11-16 19:35:29 +0100

    rep

--- Star/src/Computer/PriceList.java
+++ Star/src/Computer/PriceList.java
@@ -3,5 +3,49 @@ package Computer;
 public class PriceList  {
     //wypisanie usług z cenami
     //Diagnosis pobiera ceny stąd
-    //
+    private int Replace;
+    private int Instalation;
+    private int Building;
+    private int Cleaning;
+    private int Reparing;
+
+    public int getReplace() {
+        return Replace;
+    }
+
+    public void setReplace(int replace) {
+        Replace = replace;
+    }
+
+    public int getInstalation() {
+        return Instalation;
+    }
+
+    public void setInstalation(int installation) {
+        Instalation = installation;
+    }
+
+    public int getBuilding() {
+        return Building;
+    }
+
+    public void setBuilding(int building) {
+        Building = building;
+    }
+
+    public int getCleaning() {
+        return Cleaning;
+    }
+
+    public void setCleaning(int cleaning) {
+        Cleaning = cleaning;
+    }
+
+    public int getReparing() {
+        return Reparing;
+    }
+
+    public void setReparing(int reparing) {
+        Reparing = reparing;
+    }
 }
