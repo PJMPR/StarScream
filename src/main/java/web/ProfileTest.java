3a77707826e71e5e320c337f85c626abcf7d947f
ProfSJonalista <jan.mietki@gmail.com>
2016-12-18 18:09:32 +0100

    rep

--- src/main/java/web/ProfileTest.java
+++ src/main/java/web/ProfileTest.java
@@ -5,7 +5,7 @@ import Computer.Profile;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
