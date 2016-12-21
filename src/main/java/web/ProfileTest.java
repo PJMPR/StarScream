1b9f973f8b1fc91da7d1932cca21479e1225da95
ProfSJonalista <jan.mietki@gmail.com>
2016-12-21 11:36:53 +0100

    Added Jetty

--- src/main/java/web/ProfileTest.java
+++ src/main/java/web/ProfileTest.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns="/addProfile")
-=======
 public class ProfileTest extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
